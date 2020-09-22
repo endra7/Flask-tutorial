167e3d7ae65aa5389f91953786a4ea77de680c9c
Indra Pun <indrapun21@gmail.com>
2020-09-22 12:13:26 +1000

    URL Building using redirect and url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,21 @@ def show_blog(postID):
 @app.route('/rev/<float:revNo>')
 def revision(revNo):
     return 'Revision Number %f' % revNo
+
+#URL building
+# Greet admin as is. Greet others as guest
+@app.route('/admin')
+def admin():
+    return 'Hello admin'
+@app.route('/guest/<guest>')
+def guest(guest):
+    return 'Hello %s as Guest' % guest
+@app.route('/user/<name>')
+def user(name):
+    if name=='admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest',guest=name))
 
 if __name__=='__main__':
     app.debug=True
